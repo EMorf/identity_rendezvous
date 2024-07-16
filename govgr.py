31faf26a137094c31c3a93c3394124caea34c4d6
Emmanuel Morfiadakis <emmanuel.morfiadakis@gmail.com>
2024-07-16 18:12:01 +0300

    feat: added code to get to login form:

--- govgr.py
+++ govgr.py
@@ -1,10 +1,19 @@
 from mechanize import Browser, Link
 import re
 
-br = Browser()
-br.open(
-    "https://www.gov.gr/ipiresies/polites-kai-kathemerinoteta/ex-apostaseos-exuperetese-politon/id"
-)
-response: Link = br.find_link(text_regex=re.compile("Είσοδος στην υπηρεσία"))
-print(response.absolute_url)
-response
+
+
+
+def transition_to_main_page() -> list:
+    br = Browser()
+    br.open(
+        "https://www.gov.gr/ipiresies/polites-kai-kathemerinoteta/ex-apostaseos-exuperetese-politon/id"
+    )
+    response: Link = br.find_link(text_regex=re.compile("Είσοδος στην υπηρεσία"))
+    url = br.follow_link(response)
+    return list(br.forms())
+
+if __name__ == "__main__":
+    form = transition_to_main_page()[0]
+    signin_list = form["username"]
+    print(form.read())
