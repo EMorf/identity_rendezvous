dfb24b0070829015ed67d8af13ff1e41c1752e47
Emmanuel Morfiadakis <emmanuel.morfiadakis@gmail.com>
2024-07-16 17:30:57 +0300

    ci: lit with ruff

--- govgr.py
+++ govgr.py
@@ -2,7 +2,9 @@ from mechanize import Browser, Link
 import re
 
 br = Browser()
-br.open("https://www.gov.gr/ipiresies/polites-kai-kathemerinoteta/ex-apostaseos-exuperetese-politon/id")
-response : Link = br.find_link(text_regex=re.compile("Είσοδος στην υπηρεσία"))
+br.open(
+    "https://www.gov.gr/ipiresies/polites-kai-kathemerinoteta/ex-apostaseos-exuperetese-politon/id"
+)
+response: Link = br.find_link(text_regex=re.compile("Είσοδος στην υπηρεσία"))
 print(response.absolute_url)
 response
